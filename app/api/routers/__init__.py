1ba225f83726e4296669ab88e01f94eda9e8dd40
ガチャポンピン <148433127+gatyaponpin@users.noreply.github.com>
2025-10-27 19:23:19 +0900

    Delete app directory

--- app/api/routers/__init__.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI
-from .health import router as health_router
-from .categories import router as categories_router
-from .kakeibo import router as kakeibo_router
-
-def include_all_routers(app: FastAPI):
-    app.include_router(health_router)
-    app.include_router(categories_router)
-    app.include_router(kakeibo_router)
